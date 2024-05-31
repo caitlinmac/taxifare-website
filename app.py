ee095a13ddd98e0b44206e88f05a058101448713
Caitlin Mac <caitlin.maceachen@gmail.com>
2024-05-31 15:28:56 -0400

    2nd commit - small changes

--- app.py
+++ app.py
@@ -3,23 +3,39 @@ import datetime
 import requests
 
 '''
-# TaxiFareModel front
+# Taxi Fare Calculator
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
+
 
 "### Please provide the following information about your taxi ride:"
+st.markdown('''
+When would you like to be picked up?
+''')
 date = st.date_input(
     "Date",datetime.date(2014,7,6))
 time = st.time_input('Time', datetime.time(19, 18))
+st.markdown('''
+What is your pickup location?
+''')
 pickup_longitude = st.text_input('Pickup longitude',-73.950655)
 pickup_latitude = st.text_input('Pickup latitude',40.783282)
+st.markdown('''
+What is your dropoff location?
+''')
 dropoff_longitude = st.text_input('Dropoff longitude',-73.984365)
 dropoff_latitude = st.text_input('Dropoff latitude',40.769802)
+st.markdown('''
+How many passengers will you be?
+''')
 passenger_count = st.text_input('Passenger count',2)
+
+st.markdown('''
+
+
+
+Let's make sure we have the right info:
+''')
 
 st.write(f'Your taxi ride would be from ({pickup_longitude},{pickup_latitude}) to ({dropoff_longitude},{dropoff_latitude}) for {passenger_count} passenger(s) on {date} at {time}')
 
